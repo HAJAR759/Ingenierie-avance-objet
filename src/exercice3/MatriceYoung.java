a62ca213bc9da5955a93ff3f812758e950ec4c47
mouna <mounaettalbi@gmail.com>
2024-10-12 19:07:14 +0100

    edit class MatriceYoung

--- src/exercice3/MatriceYoung.java
+++ src/exercice3/MatriceYoung.java
@@ -37,63 +37,8 @@ public class MatriceYoung  extends ArrayList<Object> {
             }
         }
     }
-    public int extraireMin(){
-        /*Aucun élément n'est inséré dans le tableau*/
-        if(this.getElement(0,0) == INF ){
-            return INF;
-        }
-        else{
-            int min = this.getElement(0,0);
-            equilibrerTableau(0,0);
-            return min;
-        }
-    }
-    @Override
-    public boolean add(Object o){
-        int key = (Integer)(o);
-        if (getElement(n - 1, m-1) != INF) {
-            System.out.println("Le tableau de Young est plein.");
-            return false;
-        }
-        // Insérer la clé dans la dernière position (case en bas à droite)
-            this.setElement(n-1, m-1, key);
-        // Maintenir la propriété du tableau de Young
-        int i = n - 1;
-        int j = m - 1;
-        while (i > 0 || j > 0) {
-            int top = (i > 0) ? this.getElement(i - 1,j-1) : INF;
-            int left = (j > 0) ? this.getElement(i - 1,j) : INF;
-
-            if (key < top && key < left) {
-                break;  // Key is in the right place
-            } else if (top < left) {
-                this.setElement(i - 1,j, top);
-                i--;
-            } else {
-                this.setElement(i - 1,j, left);
-                j--;
-            }
-        }
-        this.setElement(i - 1,j,key);
-        return true;
-    }
-    // Fonction pour rétablir la propriété du tableau de Young après l'extraction du minimum
-    private void equilibrerTableau(int i , int j){
-        int down = (i + 1 < n) ? this.getElement(i + 1,j) : INF;
-        int right = (j + 1 < m) ? this.getElement(i,j + 1) : INF;
-
-        if (down == INF && right == INF) {
-            return; // Fin de la descente
-        }
-
-        if (down < right) {
-            this.setElement(i,j,down);
-            equilibrerTableau(i + 1, j);
-        } else {
-            this.setElement(i,j,right);
-            equilibrerTableau(i, j + 1);
-        }
-    }
+    
+   
     public void printTabbleau(){
         for(int i = 0; i < n; i++){
             for(int j= 0; j< m; j++){
@@ -108,10 +53,10 @@ public class MatriceYoung  extends ArrayList<Object> {
             System.out.println();
         }
     }
-    private int getElement(int i,int j){
+    public int getElement(int i,int j){
         return ((ArrayList<Integer>) this.get(i)).get(j);
     }
-    private void setElement(int i, int j , int valeur){
+    public void setElement(int i, int j , int valeur){
         ((ArrayList<Integer>) this.get(i)).set(j,valeur);
     }
 }
