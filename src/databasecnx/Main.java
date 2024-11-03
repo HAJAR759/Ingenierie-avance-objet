6f7f8fcb0bbf93dcc86475a0b99a3b87939c9e7a
mouna <mounaettalbi@gmail.com>
2024-11-03 12:45:35 +0100

    modify Main class

--- src/databasecnx/Main.java
+++ src/databasecnx/Main.java
@@ -7,15 +7,16 @@ public class Main {
     public static void main(String[] args) {
         // Tentative de création de plusieurs instances
         DatabaseConnection db1 = DatabaseConnection.getInstance();
-        System.out.println("Temps de création de la première instance : " + db1.getCreationTime() + " ms");
-
-        // Exécution d'une requête
-        db1.executeQuery("SELECT * FROM users");
-
-        // Nouvelle tentative de création de connexion
         DatabaseConnection db2 = DatabaseConnection.getInstance();
+        
+        System.out.println("Temps de création de la première instance : " + db1.getCreationTime() + " ms");
         System.out.println("Temps de création de la deuxième instance : " + db2.getCreationTime() + " ms");
 
+        
+        db1.executeQuery("SELECT * FROM users");
+        db2.executeQuery("INSERT INTO users VALUES ('mouna','hajar')");
+
+        
         // Vérification que les instances sont identiques
         System.out.println("Les instances sont-elles identiques ? " + (db1 == db2));
     }
